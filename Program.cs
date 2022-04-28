0372e62eab69ef8ea24bbfb85ca290cf53a67c4a
coolness10101 <aaron@missionarymall.org>
2022-04-28 11:40:55 -0600

    removed faulty print function, doewsn't work with values

--- Program.cs
+++ Program.cs
@@ -4,21 +4,13 @@ namespace tictactoe
 {
     class Program
     {
-        static void print(params string[] args){
-            foreach (string str in args){
-                Console.Write(str+' ');
-            }
-            Console.Write('\n');
-        }
         static void Main(string[] args)
         {
-            int width = 3;
-            int height = 3;
-            bool[,] board = new bool[width,height];
+            int size = 3;
+            bool[,] board = new bool[size,size];
             board[0,0] = true;
-            print("ok","three","hello","cool");
-            for (int r = 0; r < height; r++){
-                for (int c = 0; c < width; c++){
+            for (int r = 0; r < size; r++){
+                for (int c = 0; c < size; c++){
                     
                 }
             }
